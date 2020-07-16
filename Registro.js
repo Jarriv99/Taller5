a3b10e5408cad6f98161b585770b0249fa1e2176
Jarriv <brajhan1@hotmail.com>
2020-07-16 17:01:06 -0500

    corfirm password fixed!!

--- Registro.js
+++ Registro.js
@@ -1,10 +1,20 @@
-function showContent() {
-    element = document.getElementById("content");
+function VerMas() {
+    element = document.getElementById("show");
     check = document.getElementById("check");
     if (check.checked) {
         element.style.display='block';
     }
     else {
         element.style.display='none';
+    }
+}
+function PassValidation(){
+	var pass = document.getElementById("pass");
+    var passconfirm = document.getElementById("confirmpass");
+    if (pass.value == passconfirm.value) {
+        document.getElementById("invalid-pass").style.display = "none";
+    } else {
+        document.getElementById("invalid-pass").style.display = "block";
+        passconfirm.value = '';
     }
 }
