1d9029a736c6d0ef5f289c523f90d4335fa6f76d
Jarriv <brajhan1@hotmail.com>
2020-07-16 16:44:01 -0500

    ver más add

--- Registro.js
+++ Registro.js
@@ -1,13 +1,10 @@
-$(function() {
-    $( "#slider-range" ).slider({
-      range: true,
-      min: 0,
-      max: 500,
-      //values: [ 75, 300 ],
-      slide: function( event, ui ) {
-        $( "#amount" ).val( "$" + ui.values[ 0 ] ); //+ " - $" + ui.values[ 1 ] );
-      }
-    });
-    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) );// +
-     // " - $" + $( "#slider-range" ).slider( "values", 1 ) );
-  } );
+function showContent() {
+    element = document.getElementById("content");
+    check = document.getElementById("check");
+    if (check.checked) {
+        element.style.display='block';
+    }
+    else {
+        element.style.display='none';
+    }
+}
