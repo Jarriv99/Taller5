ea9b95fd34da907b21b32ddb318fa33b11a735ae
Jarriv <brajhan1@hotmail.com>
2020-07-16 19:07:35 -0500

    characters validation added!! finished.

--- Registro.js
+++ Registro.js
@@ -1,3 +1,31 @@
+function ValidationName(){
+	var charactersN = document.getElementById("name");
+    if (charactersN.value.length <= 25) {
+        document.getElementById("invalid-name").style.display = "none";
+    } else {
+        document.getElementById("invalid-name").style.display = "block";
+        charactersN.value = '';
+    }
+}
+function ValidationLN(){
+	var charactersL = document.getElementById("lastname");
+    if (charactersL.value.length <= 25) {
+        document.getElementById("invalid-lastname").style.display = "none";
+    } else {
+        document.getElementById("invalid-lastname").style.display = "block";
+        charactersL.value = '';
+    }
+}
+function UserName(){
+	var User = document.getElementById("UN");
+    if (User.value.length >= 10 && User.value.length <= 20) {
+        document.getElementById("invalid-UN").style.display = "none";
+    } else {
+        document.getElementById("invalid-UN").style.display = "block";
+        User.value = '';
+    }
+}
+
 var Address = ['cll', 'cra', 'av', 'anv', 'trans'];
 
 function AddressValidation() {
@@ -39,7 +67,8 @@ function PassValidation(){
     } else {
         document.getElementById("invalid-pass").style.display = "block";
         passconfirm.value = '';
-    }
+    };
+    
 }
 function PassValidationChar(){
 	var pass = document.getElementById("pass");
